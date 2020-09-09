910e9b131e08038aac5eab57add05c87846a703e
segawaMike <collinsmicheal256@gmail.com>
2020-09-09 06:24:15 +0300

    Updates

---
+++ precache-manifest.f831cf4330eefd2820e41850d5c5fbcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87269ce288b25f5326958ec41a2a8811",
+    "url": "/copa/index.html"
+  },
+  {
+    "revision": "aca756312568b39a4e01",
+    "url": "/copa/static/css/main.ec64bb89.chunk.css"
+  },
+  {
+    "revision": "b37c962f1968a815e338",
+    "url": "/copa/static/js/2.500d0274.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/copa/static/js/2.500d0274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca756312568b39a4e01",
+    "url": "/copa/static/js/main.0b38f1a2.chunk.js"
+  },
+  {
+    "revision": "ca315332cc9a2193045f",
+    "url": "/copa/static/js/runtime-main.d121fc8b.js"
+  }
+]);
